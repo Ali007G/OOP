eb51d7bcb1296739d769590efe063e35f0abb1dd
Ali <95523475+Ali007G@users.noreply.github.com>
2023-09-30 21:02:58 +0600

    Update 14. ConstantMemberFunction

--- OOP All Slides Example Code/14. ConstantMemberFunction.cpp
+++ OOP All Slides Example Code/14. ConstantMemberFunction.cpp
@@ -1,12 +1,11 @@
 // const
-
 #include<iostream>
 using namespace std;
 
 class Something {
 public:
     int m_value;
-    void resetValue() {m_value=0;}
+    void resetValue(){m_value=0;}
     void setValue(int value) {m_value=value;}
 
     int getValue() const {return m_value;}
